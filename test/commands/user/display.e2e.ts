5cf90577f32455ff73862b14ff1a1f427859ce62
mshanemc <shane.mclaughlin@salesforce.com>
2020-12-21 08:23:06 -0600

    revert: test folder structure

--- test/commands/user/display.e2e.ts
+++ test/commands/user/display.e2e.ts
@@ -32,8 +32,6 @@ describe('UserDisplay tests', () => {
   // some tests run locally (operate on metadata, use filesystem, but don't need an org)
   // this flag makes the org optional
   if (!process.env.LOCALONLY) {
-    // jest.setTimeout(testutils.remoteTimeout);
-
     before(async () => {
       await fs.remove(testProjectName);
       await exec(`sfdx force:project:create -n ${testProjectName}`);
@@ -44,32 +42,32 @@ describe('UserDisplay tests', () => {
       const commandResult = await exec2JSON('sfdx force:user:display --json', {
         cwd: testProjectName,
       });
-      // console.log(JSON.stringify(commandResult.result.searchRecords));
       expect(meetsRequiredShape(commandResult)).to.equal(true);
       expect(commandResult.result).not.to.have.property('alias');
       expect(commandResult.result).not.to.have.property('password');
     });
 
     it('includes the password and alias', async () => {
-      const passwordResult = await exec2JSON('sfdx force:user:password:generate', {
+      const passwordResult = await exec2JSON('sfdx force:user:password:generate --json', {
         cwd: testProjectName,
       });
+      expect(passwordResult.status).equal(0);
       const commandResult = await exec2JSON('sfdx force:user:display --json', {
         cwd: testProjectName,
       });
-      // console.log(JSON.stringify(commandResult.result.searchRecords));
       expect(meetsRequiredShape(commandResult)).to.equal(true);
-      expect(commandResult.result).to.be.an('object').with.property('username').equal(passwordResult.result.username);
+      expect(commandResult.result).to.be.an('object').with.property('username');
       expect(commandResult.result).to.have.property('password').equal(passwordResult.result.password);
       expect(commandResult.result).not.to.have.property('alias');
 
       const testAlias = 'testAlias';
-      await exec(`sfdx alias:set ${commandResult.result.username as string}=${testAlias}`);
+      await exec(`sfdx alias:set ${testAlias}=${commandResult.result.username as string}`);
       const commandResultWithAlias = await exec2JSON('sfdx force:user:display --json', {
         cwd: testProjectName,
       });
+
       expect(meetsRequiredShape(commandResultWithAlias)).to.equal(true);
-      expect(commandResultWithAlias.result).not.to.have.property('alias').equal(testAlias);
+      expect(commandResultWithAlias.result).to.have.property('alias').equal(testAlias);
     });
 
     after(async () => {
