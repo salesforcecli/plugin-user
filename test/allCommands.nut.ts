b9a7dc946b63cd18b1be5a6909a849b92028d414
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2021-03-08 10:00:36 -0600

    Sm/test branch (#69)
    * test: nuts on circle
    
    * test: orb, take 2
    
    * chore: bump core version to get password fix
    
    * test: remove call-count checks (core changed)
    
    * chore: bump core version lockfile
    
    * test: bump orb to re-test 2
    
    * test: iterator to re-run circle
    
    * test: run nuts on windows
    
    * test: restore linux nut label
    
    * test: re-run to try new windows job
    
    * test: add windows as os parameter
    
    * test: remove parameters
    
    * test: verify orb runs without windows
    
    * test: nut try 5
    
    * test: nuts without windows
    
    * test: nut with windows
    
    * chore: bump prettier for windows EOL
    
    * test: yarn install 2x for dev-scripts
    
    * chore: bump dev-scripts
    
    * chore: bump prettier AFTER dev-scripts
    
    * chore: bump mocha
    
    * test: windows-friendly paths
    
    * chore: bump setPassword to feature
    
    * test: are homeDir unique?
    
    * test: run only one nut (windows home test)
    
    * chore: bump testkit for windows fix
    
    * chore: reinstall node modules
    
    * test: resume skipped tests (check windows parallel)
    
    * test: saving artifacts
    
    * test: save zip to artifacts relative
    
    * test: windows debugging artifacts
    
    * test: also debug linux for comparison
    
    * test: nut version, debug
    
    * test: no env in config
    
    * test: test windows path fix in nut
    
    * test: artifacts folder in home
    
    * test: remove os import
    
    * test: revert to artifacts (linux only)
    
    * test: with setupCommands
    
    * test: try/catch on the zip
    
    * test: remove unused import
    
    * test: comit to cause a run after PR
    
    * test: depend on env to control zips
    
    * test: change to trigger run
    
    * chore: bumps lockfile
    
    * test: environment in job?
    
    * test: no env in job
    
    * test: with env set on linux
    
    * test: clean up
    
    * test: don't require nuts on publish job
    
    * refactor: typing on NUTs
    
    * test: use published orb with nuts
    
    * test: add version
    
    * test: rerun circle with orb nuts

--- test/allCommands.nut.ts
+++ test/allCommands.nut.ts
@@ -4,6 +4,7 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+import * as path from 'path';
 import { use, expect } from 'chai';
 import * as chaiEach from 'chai-each';
 
@@ -12,6 +13,7 @@ import { TestSession, execCmd } from '@salesforce/cli-plugins-testkit';
 import { PermsetAssignResult } from '../src/commands/force/user/permset/assign';
 import { AuthList } from '../src/commands/force/user/list';
 import { UserCreateOutput } from '../src/commands/force/user/create';
+import { UserDisplayResult } from '../src/commands/force/user/display';
 
 use(chaiEach);
 let session: TestSession;
@@ -20,38 +22,48 @@ describe('verifies all commands run successfully ', () => {
   before(() => {
     session = TestSession.create({
       project: {
-        sourceDir: 'test/df17AppBuilding',
+        sourceDir: path.join('test', 'df17AppBuilding'),
       },
       // create org and push source to get a permset
-      setupCommands: ['sfdx force:org:create -d 1 -s -f config/project-scratch-def.json', 'sfdx force:source:push'],
+      setupCommands: [
+        `sfdx force:org:create -d 1 -s -f ${path.join('config', 'project-scratch-def.json')}`,
+        'sfdx force:source:push',
+      ],
     });
   });
 
   it('user display', () => {
-    const output = execCmd('force:user:display --json', { ensureExitCode: 0 });
-    expect(output.jsonOutput)
-      .to.have.property('result')
-      .with.all.keys(['username', 'accessToken', 'id', 'orgId', 'profileName', 'loginUrl', 'instanceUrl']);
-    const result = (output.jsonOutput as Record<string, unknown>).result as Record<string, string>;
-    expect(result.orgId).to.have.length(18);
-    expect(result.id).to.have.length(18);
-    expect(result.accessToken.startsWith(result.orgId.substr(0, 15))).to.be.true;
+    const output = execCmd<UserDisplayResult>('force:user:display --json', { ensureExitCode: 0 }).jsonOutput;
+    expect(output.result).to.have.all.keys([
+      'username',
+      'accessToken',
+      'id',
+      'orgId',
+      'profileName',
+      'loginUrl',
+      'instanceUrl',
+    ]);
+
+    expect(output.result.orgId).to.have.length(18);
+    expect(output.result.id).to.have.length(18);
+    expect(output.result.accessToken.startsWith(output.result.orgId.substr(0, 15))).to.be.true;
   });
 
   it('assigns a permset to the default user', () => {
-    const output = execCmd('force:user:permset:assign -n VolunteeringApp --json', { ensureExitCode: 0 });
-    expect(output.jsonOutput).to.have.property('result').with.all.keys(['successes', 'failures']);
-    const result = (output.jsonOutput as Record<string, unknown>).result as PermsetAssignResult;
-    expect(result.successes).to.have.length(1);
-    expect(result.successes[0]).to.have.all.keys(['name', 'value']);
-    expect(result.failures).to.have.length(0);
+    const output = execCmd<PermsetAssignResult>('force:user:permset:assign -n VolunteeringApp --json', {
+      ensureExitCode: 0,
+    }).jsonOutput;
+    expect(output.result).to.have.all.keys(['successes', 'failures']);
+    expect(output.result.successes).to.have.length(1);
+    expect(output.result.successes[0]).to.have.all.keys(['name', 'value']);
+    expect(output.result.failures).to.have.length(0);
   });
 
   it('creates a secondary user', () => {
-    const output = execCmd('force:user:create --json -a Other', { ensureExitCode: 0 });
-    expect(output.jsonOutput).to.have.property('result').with.all.keys(['orgId', 'permissionSetAssignments', 'fields']);
-    const result = (output.jsonOutput as Record<string, unknown>).result as UserCreateOutput;
-    expect(result.fields).to.have.all.keys(
+    const output = execCmd<UserCreateOutput>('force:user:create --json -a Other', { ensureExitCode: 0 }).jsonOutput;
+
+    expect(output.result).to.have.all.keys(['orgId', 'permissionSetAssignments', 'fields']);
+    expect(output.result.fields).to.have.all.keys(
       'id',
       'username',
       'alias', // because it's set in the command
@@ -66,20 +78,21 @@ describe('verifies all commands run successfully ', () => {
   });
 
   it('assigns permset to the secondary user', () => {
-    const output = execCmd('force:user:permset:assign -n VolunteeringApp --json --onbehalfof Other', {
-      ensureExitCode: 0,
-    });
-    expect(output.jsonOutput).to.have.property('result').with.all.keys(['successes', 'failures']);
-    const result = (output.jsonOutput as Record<string, unknown>).result as PermsetAssignResult;
-    expect(result.successes).to.have.length(1);
-    expect(result.successes[0]).to.have.all.keys(['name', 'value']);
-    expect(result.failures).to.have.length(0);
+    const output = execCmd<PermsetAssignResult>(
+      'force:user:permset:assign -n VolunteeringApp --json --onbehalfof Other',
+      {
+        ensureExitCode: 0,
+      }
+    ).jsonOutput;
+    expect(output.result).to.have.all.keys(['successes', 'failures']);
+    expect(output.result.successes).to.have.length(1);
+    expect(output.result.successes[0]).to.have.all.keys(['name', 'value']);
+    expect(output.result.failures).to.have.length(0);
   });
 
   it('lists the users', () => {
-    const output = execCmd('force:user:list --json', { ensureExitCode: 0 });
-    expect(output.jsonOutput).to.have.property('result').to.be.an('array').with.length(2);
-    const result = (output.jsonOutput as Record<string, unknown>).result as AuthList[];
+    const result = execCmd<AuthList[]>('force:user:list --json', { ensureExitCode: 0 }).jsonOutput.result;
+    expect(result).to.be.an('array').with.length(2);
     expect(result).each.have.all.keys([
       'defaultMarker',
       'alias',
@@ -94,16 +107,17 @@ describe('verifies all commands run successfully ', () => {
   });
 
   it('generates new passwords for main user', () => {
-    const output = execCmd('force:user:password:generate --json', { ensureExitCode: 0 });
-    expect(output.jsonOutput).to.have.property('result').includes.keys(['username', 'password']);
+    const output = execCmd('force:user:password:generate --json', { ensureExitCode: 0 }).jsonOutput;
+    expect(output).to.have.property('result').includes.keys(['username', 'password']);
   });
 
   it('generates new password for secondary user (onbehalfof)', () => {
-    const output = execCmd('force:user:password:generate -o Other --json', { ensureExitCode: 0 });
-    expect(output.jsonOutput).to.have.property('result').includes.keys(['username', 'password']);
+    const output = execCmd('force:user:password:generate -o Other --json', { ensureExitCode: 0 }).jsonOutput;
+    expect(output).to.have.property('result').includes.keys(['username', 'password']);
   });
 
   after(async () => {
+    await session.zip(undefined, 'artifacts');
     await session.clean();
   });
 });
