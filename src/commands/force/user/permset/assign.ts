67b8e60990eb28c55660b2842316d5a48d3ad6a0
mshanemc <shane.mclaughlin@salesforce.com>
2021-11-11 10:21:36 -0600

    refactor: fix TS update errors

--- src/commands/force/user/permset/assign.ts
+++ src/commands/force/user/permset/assign.ts
@@ -78,6 +78,9 @@ export class UserPermsetAssignCommand extends SfdxCommand {
         }
       }
     } catch (e) {
+      if (!(e instanceof SfdxError)) {
+        throw e;
+      }
       throw SfdxError.wrap(e);
     }
 
