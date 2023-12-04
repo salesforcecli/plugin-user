708893d6cea0143565706517c7cd1671bc3d35a4
Mike Donnalley <mdonnalley@salesforce.com>
2023-12-04 12:12:04 -0800

    test: expect nonZero exit code (#810)

--- test/forceCommands.nut.ts
+++ test/forceCommands.nut.ts
@@ -174,11 +174,15 @@ describe('verifies legacy force commands run successfully ', () => {
   });
 
   it('generates new password for secondary user (onbehalfof) with complexity 7 should thrown an error', () => {
-    const output = execCmd('force:user:password:generate -o Other --json -c 7', { ensureExitCode: 1 }).jsonOutput;
+    const output = execCmd('force:user:password:generate -o Other --json -c 7', {
+      ensureExitCode: 'nonZero',
+    }).jsonOutput;
     expect(output?.message).to.include('Expected an integer less than or equal to 5 but received: 7');
   });
   it('generates new password for secondary user (onbehalfof) with length 7 should thrown an error', () => {
-    const output = execCmd('force:user:password:generate -o Other --json -l 7', { ensureExitCode: 1 }).jsonOutput;
+    const output = execCmd('force:user:password:generate -o Other --json -l 7', {
+      ensureExitCode: 'nonZero',
+    }).jsonOutput;
     expect(output?.message).to.include('Expected an integer greater than or equal to 8 but received: 7');
   });
   it('assigns 2 permsets to the main user', () => {
